b8adc943a03ee654b731d6df5def8b993f16300a
Ishan Bista <125456778+ishanbista@users.noreply.github.com>
2025-03-06 20:56:46 +0545

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ const checkWinner = () => {
             if (pos1val == pos2val && pos2val == pos3val) {
 
                 showWinner(pos1val);
+                return;
             }
 
         }
